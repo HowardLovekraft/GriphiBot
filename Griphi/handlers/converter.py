bcef1f22485b6f507e7e9bbb8f03142a9271f40c
HowardLovekraft <117375241+HowardLovekraft@users.noreply.github.com>
2023-05-14 17:10:08 +0500

    Update model
    > Change model
    > Try use GPU instead of CPU
    > Move code_genetator to a separate folder

--- Griphi/handlers/converter.py
+++ Griphi/handlers/converter.py
@@ -8,13 +8,13 @@ from aiogram.methods.get_file import GetFile
 
 from Griphi.env.env_reader import get_token
 from Griphi.speech.app import get_transcribe
-from Griphi.code_generator import generator
+from Griphi.other.code_generator import generator
 
 
+bot = Bot(token=get_token())
 router = Router()
 i18n = I18n(path="locales", default_locale="en", domain="messages")
 router.message.middleware(SimpleI18nMiddleware(i18n))
-bot = Bot(token=get_token())
 
 @router.message(Command("start"))
 async def start(message: Message) -> None:
@@ -34,6 +34,6 @@ async def echo_inline(message: Message):
     await message.reply(text=_("I got ur voice message. Please, wait..."))
 
     audio_file_name = f"{code}.mp3"
-    transcribe = get_transcribe(audio_file_name)
-    await message.answer(text=transcribe)
+    transcribe = await get_transcribe(audio_file_name)
     os.remove(path)
+    await message.answer(text=transcribe)
--- Griphi/speech/app.py
+++ Griphi/speech/app.py
@@ -3,9 +3,9 @@ import whisper
 
 BASE_DIR = "C:\\GitHub\\GriphiBot\\Griphi\\speech"
 print(BASE_DIR)
-model = whisper.load_model("base")
+model = whisper.load_model("small").cuda()
 
-def get_transcribe(name):
+async def get_transcribe(name):
     result = model.transcribe(os.path.join(BASE_DIR, name), fp16=False)
     return result["text"]
 
