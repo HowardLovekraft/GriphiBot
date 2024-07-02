44855104a6e73241e08f565512902be71d581334
HowardLovekraft <117375241+HowardLovekraft@users.noreply.github.com>
2024-07-02 18:44:48 +0500

    feat: QoL updates
    > Bot don't need confirmation to start transcribing

--- Griphi/handlers/converter.py
+++ Griphi/handlers/converter.py
@@ -19,81 +19,57 @@ router.message.middleware(SimpleI18nMiddleware(i18n))
 router.callback_query.middleware(SimpleI18nMiddleware(i18n))
 
 
-class Transcriber(StatesGroup):
-    work_media = State()
+async def work_media(message: Message, file_path) -> None:
+    code = await generator()
+    audio_file_name = f"{code}.mp3"
+    path = f"{BASE_DIR}\\{audio_file_name}"
+
+    await bot.download_file(file_path=file_path,
+                            destination=path,
+                            timeout=90,
+                            chunk_size=262144)
+    reply_msg = await message.reply(text=_("I downloaded your file. Start transcribing..."))
+
+    transcribe = await get_transcribe(audio_file_name)
+    os.remove(path)
+    await reply_msg.edit_text(text=transcribe)
+
 
 @router.message(Command("start"))
-async def start(message: Message, state: FSMContext) -> None:
+async def start(message: Message) -> None:
     await message.answer(_("Hello, I'm Griphi.\nI can convert your voice messages and video notes to text.\n"
                            "To start, just forward to me a media file"))
 
 
 @router.message(F.voice)
-async def get_voice(message: Message, state: FSMContext) -> None:
+async def get_voice(message: Message) -> None:
     file_id = message.voice.file_id
     file = await bot.get_file(file_id=file_id)
+    file_path = file.file_path
 
-    data = await state.get_data()
-    data["file"] = file.file_path
-    await state.set_data(data)
+    await work_media(message, file_path)
 
-    await message.reply(text=_("I got ur voice message. Do u want to start transcribing?"),
-                        reply_markup=get_accept())
-    await state.set_state(Transcriber.work_media)
 
 @router.message(F.video_note)
-async def get_video_note(message: Message, state: FSMContext) -> None:
+async def get_video_note(message: Message) -> None:
     file_id = message.video_note.file_id
     file = await bot.get_file(file_id=file_id)
+    file_path = file.file_path
 
-    data = await state.get_data()
-    data["file"] = file.file_path
-    await state.set_data(data)
-    await message.reply(text=_("I got ur video note. Do u want to start transcribing?"),
-                        reply_markup=get_accept())
-    await state.set_state(Transcriber.work_media)
+    await work_media(message, file_path)
+
 
 # refactor it! Move it into get_voice function.
 # remember about DRY.
 @router.message(F.video)
-async def get_video_file(message: Message, state: FSMContext) -> None:
+async def get_video_file(message: Message) -> None:
     file_id = message.video.file_id
     file = await bot.get_file(file_id=file_id)
-    data = await state.get_data()
-    data["file"] = file.file_path
-    await state.set_data(data)
-    await message.reply(text="I got ur video file. Do u want to start transcribing?",
-                        reply_markup=get_accept())
-    await state.set_state(Transcriber.work_media)
+    file_path = file.file_path
 
-@router.message(~(F.voice | F.video_note| F.video))
+    await work_media(message, file_path)
+
+
+@router.message(~(F.voice | F.video_note | F.video))
 async def get_not_media(message: Message) -> None:
-    await message.reply(_("Please, send me only voice messages/video notes"))
-
-@router.callback_query(Transcriber.work_media)
-async def work_media(callback: CallbackQuery, state: FSMContext) -> None:
-    if callback.data == "accept":
-        await callback.answer()
-        await callback.message.edit_text(_("Please wait..."))
-
-        code = await generator()
-        path = f"{BASE_DIR}\\{code}.mp3"
-        audio_file_name = f"{code}.mp3"
-
-        data = await state.get_data()
-        await bot.download_file(file_path=data["file"],
-                                destination=path,
-                                timeout=90,
-                                chunk_size=262144)
-        await callback.message.edit_text(text=_("I downloaded your file. Start transcribing..."))
-
-        transcribe = await get_transcribe(audio_file_name)
-        os.remove(path)
-
-        await callback.message.edit_text(text=transcribe)
-
-    elif callback.data == "cancel":
-        await callback.answer()
-        await callback.message.edit_text(_("You canceled the process"))
-
-    await state.clear()
+    await message.reply(_("Please, send me only:\n- voice messages\n- video notes\n- videos"))
